72685cd898e6ede5c9cb220c001ddece426b2971
daffa_p <daffaarravi@gmail.com>
2021-06-23 17:35:36 +0700

    initial commit

---
+++ src/test/java/com/happy5/app/messaging/MessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happy5.app.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
