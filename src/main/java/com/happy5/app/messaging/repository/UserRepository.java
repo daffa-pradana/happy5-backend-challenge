26727dcf5e45e0ca84bbf1338359b00dc73ab608
daffa_p <daffaarravi@gmail.com>
2021-06-23 20:52:20 +0700

    create user repo

---
+++ src/main/java/com/happy5/app/messaging/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.happy5.app.messaging.repository;
+
+import com.happy5.app.messaging.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserById(Long id);
+}
