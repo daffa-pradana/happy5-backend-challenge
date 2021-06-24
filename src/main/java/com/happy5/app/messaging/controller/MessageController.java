3f36a7ebc11f0f156891c5f32a87e8d84d4e61cc
daffa_p <daffaarravi@gmail.com>
2021-06-24 21:19:42 +0700

    create message controller

---
+++ src/main/java/com/happy5/app/messaging/controller/MessageController.java
@@ -0,0 +1,23 @@
+package com.happy5.app.messaging.controller;
+
+import com.happy5.app.messaging.service.MessageService;
+import com.happy5.app.messaging.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/message")
+public class MessageController {
+
+    private UserService userService;
+    private MessageService messageService;
+
+    // inject
+    @Autowired
+    public MessageController(UserService userService, MessageService messageService) {
+        this.userService = userService;
+        this.messageService = messageService;
+    }
+
+}
